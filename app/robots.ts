ae51f97263916c0c2d80690af66ff6251886c3c3
Eskisofrenico <franciscosaldivia87@gmail.com>
2026-01-18 22:06:36 -0300

    cambiosvs1

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next'
 
+// Requerido para output: export
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
     const baseUrl = 'https://franciscodev.cl'
 
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next'
 
+// Requerido para output: export
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://franciscodev.cl'
 
